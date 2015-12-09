2773482598c5ea69bf42ff18706667935d5611b5
Valentin Escoyez <valentin.escoyez@gmail.com>
2015-12-09 16:37:59 +0100

    seed with an update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates)
+
+seed DB with "code du travail"
+
+```
+$ rake db:create
+$ psql lexyapp_development < lexyapp.pgsql
+```
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# require "nokogiri"
+
+# code_du_travail = Law.create({name: "code du travail", legi_cid: "LEGITEXT000006072050"})
+
+# Dir.glob("db/LEGITEXT000006072050/article/**/*.xml") do |article|
+#   doc = File.open(article) { |f| Nokogiri::XML(f) }
+#   article = code_du_travail.articles.build({
+#     number: doc.xpath("//NUM").first.content,
+#     legi_id: doc.xpath("//ID").first.content
+#   })
+#   article.save
+#   version = article.versions.build({
+#     content: doc.xpath("//BLOC_TEXTUEL/CONTENU").first.content
+#   })
+#   version.save
+# end
+
 
 require "nokogiri"
 
-code_du_travail = Law.create({name: "code du travail", legi_cid: "LEGITEXT000006072050"})
+code_du_travail = Law.first
 
-Dir.glob("db/LEGITEXT000006072050/article/**/*.xml") do |article|
+Dir.glob("legi/global/code_et_TNC_en_vigueur/code_en_vigueur/LEGI/TEXT/00/00/06/07/20/LEGITEXT000006072050/article/**/*.xml") do |article|
   doc = File.open(article) { |f| Nokogiri::XML(f) }
-  article = code_du_travail.articles.build({
-    number: doc.xpath("//NUM").first.content,
-    legi_id: doc.xpath("//ID").first.content
-  })
-  article.save
-  version = article.versions.build({
-    content: doc.xpath("//BLOC_TEXTUEL/CONTENU").first.content
-  })
-  version.save
+  article_id = doc.xpath("//ID").first.content
+  article = Article.where(legi_id: article_id).first
+  unless article
+    article = code_du_travail.articles.build({
+      number: doc.xpath("//NUM").first.content,
+      legi_id: doc.xpath("//ID").first.content
+    })
+    article.save
+  end
+
+  content = doc.xpath("//BLOC_TEXTUEL/CONTENU").first.content
+  if content != article.versions.last.content
+    p article_id
+    version =  article.versions.build({
+      content: content
+    })
+    version.save
+  end
+
 end
