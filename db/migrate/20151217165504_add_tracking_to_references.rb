22c178d710a9701019b600bec32e63ef4c78465d
Valentin Escoyez <valentin.escoyez@gmail.com>
2015-12-18 12:03:04 +0100

    reference style

--- db/migrate/20151217165504_add_tracking_to_references.rb
+++ db/migrate/20151217165504_add_tracking_to_references.rb
@@ -1,5 +1,5 @@
 class AddTrackingToReferences < ActiveRecord::Migration
   def change
-    add_column :references, :tracking, :boolean
+    add_column :references, :tracking, :boolean, default: true
   end
 end
