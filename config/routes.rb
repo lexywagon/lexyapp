aa5d7d94c5bca73639a0c31e9e69d4cbd2d91c0a
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2015-12-21 13:17:37 +0100

    adding new route pages#terms

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root to: 'pages#home'
+  get '*pages', to: 'pages#terms'
+
 
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.admin } do
